af3d26e6b9c8e90301a0082e045fb63719746ab4
Sam Ceja <sammmceja@gmail.com>
2023-11-27 23:26:24 -0800

    big refactor

--- app.js
+++ app.js
@@ -1,44 +1,83 @@
-function handleSubmit(data, e) {
-  e.preventDefault();
-
-  const img = document.querySelector("img");
-  const imgSrc = getImage(data);
-
-  img.src = imgSrc;
-}
-
-async function fetchDataAndSetImage() {
+async function init() {
   const searchButton = document.querySelector("#search-button");
   const form = document.querySelector("form");
 
+  const data = await fetchData();
+
+  form.addEventListener("submit", (e) => handleSubmit(data, e));
+  searchButton.addEventListener("click", (e) => handleSubmit(data, e));
+}
+
+async function fetchData() {
   try {
     const response = await fetch("https://ghibliapi.vercel.app/films");
     const data = await response.json();
 
-    form.addEventListener("submit", (e) => handleSubmit(data, e));
-
-    searchButton.addEventListener("click", (e) => handleSubmit(data, e));
+    return data;
   } catch (error) {
     console.error("Error fetching data:", error);
   }
 }
 
-function getImage(data) {
-  const input = document.querySelector("#search-bar");
+function handleSubmit(data, e) {
+  e.preventDefault();
+
+  const img = document.querySelector("img");
+  const description = document.querySelector(".description");
   const errorMessage = document.querySelector(".error-message");
-  const searchInput = input.value.toLowerCase().replace(/'/g, "");
+
+  const info = getInfo(data);
+
+  img.src = info.image;
+  img.classList.add("border");
+  errorMessage.style.display = "none";
+
+  switch (info.status) {
+    case "ok":
+      console.log("ok");
+      description.style.visibility = "visible";
+      description.textContent = info.description;
+      return;
+
+    case "error":
+      console.log("error");
+      errorMessage.style.display = "block";
+      description.style.display = "none";
+      return;
+
+    case "none":
+    default:
+      console.log("none");
+      description.style.visibility = "hidden";
+      img.classList.remove("border");
+      return;
+  }
+}
+
+function getInfo(data) {
+  const input = document.querySelector("#search-bar");
+  const inputValue = input.value.toLowerCase().replace(/'/g, "");
+
+  if (!inputValue) {
+    return {
+      image:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/c/ca/Studio_Ghibli_logo.svg/1920px-Studio_Ghibli_logo.svg.png",
+      description: "",
+      status: "none",
+    };
+  }
 
   for (movie of data) {
     const filmTitle = movie.title.toLowerCase().replace(/'/g, "");
 
-    if (filmTitle.includes(searchInput)) {
-      errorMessage.style.visibility = "hidden";
-      return movie.image;
-    }
+    if (filmTitle.includes(inputValue)) return { ...movie, status: "ok" };
   }
 
-  errorMessage.style.visibility = "visible";
-  return "https://www.ghiblicollection.com/cdn/shop/files/download.webp?v=1687908002&width=1920";
+  return {
+    image:
+      "https://www.ghiblicollection.com/cdn/shop/files/download.webp?v=1687908002&width=1920",
+    status: "error",
+  };
 }
 
-fetchDataAndSetImage();
+init();
